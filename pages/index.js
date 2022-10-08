cf2622c6c1613ffb55160fb68c5625e4578918e9
Sammy Mwangangi <sammymwinzi2@gmail.com>
2022-10-08 18:39:07 +0300

    navabar

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,15 @@
 import Link from "next/link";
-import { Popover } from '@headlessui/react'
+import { Menu, Transition } from "@headlessui/react";
+import { Fragment, useEffect, useRef, useState } from "react";
+import { ChevronDownIcon } from "@heroicons/react/20/solid";
 
 export default function Home() {
   return (
     <div>
       <header className="py-10">
-        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div className="mx-auto max-w-7xl px-4 sm:px-2 lg:px-2">
           <nav className="relative z-50 flex justify-between">
-            <div className="flex items-center md:gap-x-12">
+            <div className="flex w-96 items-center md:gap-x-12">
               <Link href="#">
                 <svg
                   width="150"
@@ -233,18 +235,72 @@ export default function Home() {
                 </svg>
               </Link>
             </div>
-            <div className="hidden md:flex md:gap-x-6 items-center">
-              <a href="#" className="inline-block rounded-lg py-1 px-8 text-lg text-[#6D6E8A] font-semibold">
+            <div className="hidden md:flex md:gap-x-8 items-center">
+              <a
+                href="#"
+                className="inline-block py-1 px-8 text-lg text-[#6D6E8A] font-semibold"
+              >
                 Personal
               </a>
-              <a href="#" className="inline-block rounded-lg py-1 px-8 text-lg text-[#1B1C39] font-semibold">
+              <a
+                href="#"
+                className="inline-block py-1 px-8 text-lg text-[#1B1C39] font-semibold"
+              >
                 Brand Story
               </a>
             </div>
-            <div className="flex items-center gap-x-5 md:gap-x-8">
-              <a href="#" className="group inline-flex items-center justify-center rounded-full py-2 px-4 text-sm font-semibold focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 bg-blue-600 text-white hover:text-slate-100 hover:bg-blue-500 active:bg-blue-800 active:text-blue-100 focus-visible:outline-blue-600">
+            <div className="flex items-center gap-x-5 md:gap-x-8 pl-4">
+              <a
+                href="#"
+                className="group inline-flex items-center justify-center text-sm font-medium text-white"
+              >
                 <span>
-                  Get Started
+                  <svg
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <g clip-path="url(#clip0_7_234)">
+                      <path
+                        d="M12 24C18.6274 24 24 18.6274 24 12C24 5.37258 18.6274 0 12 0C5.37258 0 0 5.37258 0 12C0 18.6274 5.37258 24 12 24Z"
+                        fill="#F0F0F0"
+                      />
+                      <path
+                        d="M11.4775 12.0001H23.9993C23.9993 10.917 23.8549 9.86774 23.5859 8.86963H11.4775V12.0001Z"
+                        fill="#D80027"
+                      />
+                      <path
+                        d="M11.4775 5.73934H22.238C21.5034 4.54065 20.5642 3.48114 19.4691 2.60889H11.4775V5.73934Z"
+                        fill="#D80027"
+                      />
+                      <path
+                        d="M12.0001 24.0003C14.8243 24.0003 17.4201 23.0242 19.4699 21.3916H4.53027C6.58012 23.0242 9.17592 24.0003 12.0001 24.0003Z"
+                        fill="#D80027"
+                      />
+                      <path
+                        d="M1.76098 18.2608H22.2384C22.8281 17.2985 23.2855 16.2467 23.5863 15.1304H0.413086C0.713883 16.2467 1.17124 17.2985 1.76098 18.2608Z"
+                        fill="#D80027"
+                      />
+                      <path
+                        d="M5.55863 1.87397H6.65217L5.63498 2.61295L6.02353 3.80869L5.00639 3.0697L3.98925 3.80869L4.32487 2.7757C3.42928 3.52172 2.64431 4.39575 1.99744 5.36963H2.34783L1.70034 5.84002C1.59947 6.0083 1.50272 6.17925 1.41 6.35273L1.71919 7.30434L1.14234 6.88523C0.998953 7.18903 0.867797 7.49967 0.749906 7.81678L1.09055 8.86528H2.34783L1.33064 9.60427L1.71919 10.8L0.702047 10.061L0.0927656 10.5037C0.0317812 10.9939 0 11.4932 0 12H12C12 5.37262 12 4.59131 12 0C9.62944 0 7.41961 0.687656 5.55863 1.87397ZM6.02353 10.8L5.00639 10.061L3.98925 10.8L4.3778 9.60427L3.36061 8.86528H4.61789L5.00639 7.66955L5.39489 8.86528H6.65217L5.63498 9.60427L6.02353 10.8ZM5.63498 6.10861L6.02353 7.30434L5.00639 6.56536L3.98925 7.30434L4.3778 6.10861L3.36061 5.36963H4.61789L5.00639 4.17389L5.39489 5.36963H6.65217L5.63498 6.10861ZM10.3279 10.8L9.31073 10.061L8.29359 10.8L8.68214 9.60427L7.66495 8.86528H8.92223L9.31073 7.66955L9.69923 8.86528H10.9565L9.93933 9.60427L10.3279 10.8ZM9.93933 6.10861L10.3279 7.30434L9.31073 6.56536L8.29359 7.30434L8.68214 6.10861L7.66495 5.36963H8.92223L9.31073 4.17389L9.69923 5.36963H10.9565L9.93933 6.10861ZM9.93933 2.61295L10.3279 3.80869L9.31073 3.0697L8.29359 3.80869L8.68214 2.61295L7.66495 1.87397H8.92223L9.31073 0.678234L9.69923 1.87397H10.9565L9.93933 2.61295Z"
+                        fill="#0052B4"
+                      />
+                    </g>
+                    <defs>
+                      <clipPath id="clip0_7_234">
+                        <rect width="24" height="24" fill="white" />
+                      </clipPath>
+                    </defs>
+                  </svg>
+                </span>
+                <span className="ml-2 text-[#6D6E8A]">ENG</span>
+                <span>
+                  <ChevronDownIcon
+                    className="-mr-1 h-5 w-5 text-violet-200 hover:text-violet-100"
+                    aria-hidden="true"
+                  />
                 </span>
               </a>
             </div>
