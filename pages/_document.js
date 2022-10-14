cfe7307bef62f904732fc17121b6cca8c2d533d6
Sammy Mwangangi <sammymwinzi2@gmail.com>
2022-10-14 17:03:40 +0300

    header dev

--- pages/_document.js
+++ pages/_document.js
@@ -4,10 +4,7 @@ export default function Document() {
   return (
     <Html className='h-full scroll-smooth bg-white'>
       <Head>
-        <link
-          href="https://fonts.googleapis.com/css?family=Poppins&display=optional" 
-          rel="stylesheet"
-        />
+        
       </Head>
       <body className="flex h-full flex-col font-['Poppins']">
         <Main />
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,14 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/flowbite-react/**/*.js",
   ],
   theme: {
     extend: {
+    
     },
   },
-  plugins: [],
+  plugins: [
+    require("flowbite/plugin")
+  ],
 }
